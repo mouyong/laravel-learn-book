691b4288be99243427a71772931cec5d68d1a430
牟勇 <my24251325@gmail.com>
2017-07-09 14:54:42 +0800

    Finish login and logout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,10 @@
 <head>
     <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="/css/app.css">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
+<div id="app">
     @include('layouts._header')
     <div class="container">
         <div class="col-md-offset-1 col-md-10">
@@ -13,5 +15,8 @@
             @include('layouts._footer')
         </div>
     </div>
+</div>
+
+<script src="/js/app.js"></script>
 </body>
 </html>
